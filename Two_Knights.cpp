6aef8176985771c43e37b42b14467ba9826dcdfc
labib108 <abubuckkersiddikh@gmail.com>
2025-12-21 11:33:14 +0600

    two night solved

--- Two_Knights.cpp
+++ Two_Knights.cpp
@@ -8,9 +8,12 @@ int main() {
     int k;
     cin>>k;
     for(int i = 1; i <= k; i++){
-       long long n; 
-        n = (i*i*i*i - 9*i*i + 24*i -16)/2;
-        cout<<n<<endl;
+       long long int a,b,c,n;
+       a = (i*i);
+       b = (i*i)-1;
+       c = (i-1)*(i-2);
+       n = (a*b)/2 - 4*c;
+       cout<<n<<endl;
     }
     
     return 0;
